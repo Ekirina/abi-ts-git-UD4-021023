3d10bb86b4fad39e063b26098392e2acd6f4d79e
Ainara <ainarabliz@gmail.com>
2023-10-02 16:12:59 +0200

    Primer ejercicio

---
+++ UD4/src/javabasics1.java
@@ -0,0 +1,22 @@
+
+public class javabasics1 {
+
+	public static void main(String[] args) {
+		int num1 = 40;
+		int num2 = 2;
+		
+	int suma = num1+num2;
+	int resta = num1-num2;
+	int multiplicacion = num1*num2;
+	int division = num1/num2;
+	int resto = num1%num2;
+	
+	System.out.print(suma+" es el resultado de la suma\n"); 
+	System.out.print(resta+" es el resultado de la resta\n");
+	System.out.print(multiplicacion+" es el resultado de la multiplicación\n");
+	System.out.print(division+" es el resultado de la división\n");
+	System.out.print(resto+" es el resultado del resto\n");
+
+	}
+
+}
