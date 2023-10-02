f131ec2542e77aea8aaa6c41b5f8ee33edbe8654
Ainara <ainarabliz@gmail.com>
2023-10-02 17:36:23 +0200

    Cuarto ejercicio

---
+++ UD4/src/javabasics4.java
@@ -0,0 +1,17 @@
+
+public class javabasics4 {
+
+	public static void main(String[] args) {
+
+		int n=6;
+	
+	System.out.println("Valor inicial de n = " +n+ ".");                                                          
+        n+=77;
+    System.out.println("n + 77 = " +n+".");
+        n-=3;
+    System.out.println("n - 3 = " +n+".");
+        n*=2;
+    System.out.println("n * 2 = " +n+".");  
+	}
+ 
+}
