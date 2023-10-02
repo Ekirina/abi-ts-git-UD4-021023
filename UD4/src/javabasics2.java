52178a9b21968f3a7676c61362b5a1963e761bb4
Ainara <ainarabliz@gmail.com>
2023-10-02 16:46:01 +0200

    Segundo ejercicio

---
+++ UD4/src/javabasics2.java
@@ -0,0 +1,24 @@
+
+public class javabasics2 {
+
+	public static void main(String[] args) {
+		int n=4;
+		double a= 7;
+		char c= 'z';
+		
+	System.out.print("El valor de n es "+ n + ", el valor de a es " + a);
+	System.out.print(" y el valor de c es " +c+"\n");
+	
+		double suma = n+a;
+		double resta = a-n;
+	
+	System.out.print("El resultado de la suma de n y a es "+suma+"\n");
+	System.out.print("El resultado de la resta de a y n es "+resta+"\n");
+	
+		int valor=(int)c;
+	
+	System.out.print("el resultado del valor de c es "+valor);	
+	
+	}
+
+}
